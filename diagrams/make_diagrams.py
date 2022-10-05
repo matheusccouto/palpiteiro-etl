a8df26b8f1c36fd5b71af861f59428f096526e35
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-10-05 01:39:03 +0000

    Add flow

--- diagrams/make_diagrams.py
+++ diagrams/make_diagrams.py
@@ -10,6 +10,11 @@ from diagrams.aws.integration import StepFunctions
 from diagrams.aws.storage import S3
 from diagrams.custom import Custom
 from diagrams.gcp.analytics import Bigquery
+from diagrams.onprem.ci import GithubActions
+from diagrams.onprem.client import User
+from diagrams.onprem.vcs import Git, Github
+from diagrams.programming.language import Python
+
 
 THIS_DIR = os.path.dirname(__file__)
 ICONS_DIR = os.path.join(THIS_DIR, "icons")
@@ -147,3 +152,19 @@ with Diagram(
         >> load
         >> Bigquery("odds\nbrasileirao")
     )
+
+
+with Diagram(
+    "\nFlow",
+    filename=os.path.join(THIS_DIR, "flow"),
+    show=False,
+    curvestyle="curved",
+):
+    gh_pr = Github("pull request to main")
+    gh_merge = Github("merge to main")
+    serverless = Custom("serverless", os.path.join(ICONS_DIR, "serverless.png"))
+
+    Git("push to dev") >> gh_pr
+    gh_pr >> GithubActions("testing") >> Python("pytest") >> gh_merge
+    gh_pr >> GithubActions("linting") >> Python("pylint") >> gh_merge
+    gh_merge >> GithubActions("deploy") >> serverless
